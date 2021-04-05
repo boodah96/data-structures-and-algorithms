1c336dd95d7da55be52df1eb95e59a79866ee7c9
boodah96 <2014990065@ses.yu.edu.jo>
2021-04-05 16:26:11 +0300

    CC03

--- javascript/challenges-401/arrayBinarySearch/array-binary-search.js
+++ javascript/challenges-401/arrayBinarySearch/array-binary-search.js
@@ -1,18 +1,16 @@
 'use strict';
 
-function BinarySearch(arr, n) {
+module.exports = function BinarySearch(arr, n) {
     let output = -1;
     let length = arr.length;
     let mid;
     let start = 0;
     let end = length - 1;
     while (start <= end) {
-        console.log(start);
-        mid = parseInt((end - start) / 2);
-        console.log('mid', mid);
+        mid = parseInt(start + (end - start) / 2);
         if (n === arr[mid]) {
             output = mid;
-            start = length;
+            break;
         } else if (n < arr[mid]) {
             end = mid - 1;
         } else {
@@ -22,4 +20,3 @@ function BinarySearch(arr, n) {
     }
     return output;
 }
-console.log('index', BinarySearch([4, 8, 15], 15));
