f6c8a69f42435ed532a09cff79e3e4b4287b4aab
boodah96 <2014990065@ses.yu.edu.jo>
2021-04-14 02:55:23 +0300

    cc13 70%

---
+++ javascript/challenges-401/multiBracketValidation/multi-bracket-validation.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function multiBracketValidation(str) {
+    if (typeof str !== 'string') { return false };
+    let reg = /[{[(]/g
+    let round = 0;
+    let curly = 0;
+    let square = 0;
+    if (reg.test(str)) {
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === '(') { round++ }
+            if (str[i] === '{') { curly++ }
+            if (str[i] === '[') { square++ }
+            if (str[i] === ']' && curly === 0 && round === 0) { if (square > 0) { square-- } else { return false }; }
+            if (str[i] === '}' && square === 0 && round === 0) { if (curly > 0) { curly-- } else { return false }; }
+            if (str[i] === ')' && square === 0 && curly === 0) { if (round > 0) { round-- } else { return false }; }
+
+
+        }
+        if (round === 0 && curly === 0 && square === 0) {
+            return true;
+        } else { return false };
+    }
+    return curly;
+}
