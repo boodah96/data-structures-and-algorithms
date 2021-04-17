16cd6b2132ee46e19161bc14c463b8ce4c008c5a
boodah96 <2014990065@ses.yu.edu.jo>
2021-04-18 01:36:19 +0300

    tree1

---
+++ javascript/challenges-401/tree/README.md
@@ -0,0 +1,18 @@
+# Trees
+A tree whose elements have at most 2 children is called a binary tree. Since each element in a binary tree can have only 2 children, we typically name them the left and right child
+
+## Challenge
+- Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
+- Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+
+
+- preOrder: Root - Left - Right. 
+- inOrder : left - Root - Right. 
+- postOrder: Root - Left - Right. 
+- add method: add a value to the tree
+- contains method: return true if the tree contain the value.
--- javascript/challenges-401/tree/tree.js
+++ javascript/challenges-401/tree/tree.js
@@ -1,1 +1,62 @@
-let tree = 0;
+'use strict';
+
+class Node {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class BinaryTree {
+    constructor(root = null) {
+        this.root = root;
+    }
+
+
+    preOrder() {
+        const results = [];
+        const _traverse = (node) => {
+
+            results.push(node.value);
+
+            if (node.left) _traverse(node.left);
+
+            if (node.right) _traverse(node.right);
+        };
+        _traverse(this.root);
+        return results;
+    }
+
+    // Left - Root - Right
+    inOrder() {
+            const results = [];
+            const _traverse = (node) => {
+                // check if it have left _traverse(node.left)
+                if (node.left) _traverse(node.left);
+                //push the value to the array
+                results.push(node.value);
+                // check if it have right _traverse(node.right)
+                if (node.right) _traverse(node.right);
+            };
+            _traverse(this.root);
+            return results;
+        }
+        // left - Right - Root
+    postOrder() {
+        const results = [];
+        const _traverse = (node) => {
+            // check if it have left _traverse(node.left)
+            if (node.left) _traverse(node.left);
+            // check if it have right _traverse(node.right)
+            if (node.right) _traverse(node.right);
+            //push the value to the array
+            results.push(node.value);
+        };
+        _traverse(this.root);
+        return results;
+    }
+}
+
+module.exports = BinaryTree;
