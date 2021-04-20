cd4fed4031eb6c80827e2b008ee212efd01c2ed9
boodah96 <2014990065@ses.yu.edu.jo>
2021-04-20 04:24:34 +0300

    cc17

--- javascript/challenges-401/BinaryTree/BinaryTree.js
+++ javascript/challenges-401/BinaryTree/BinaryTree.js
@@ -66,6 +66,21 @@ class BinaryTree {
         }
         return max;
     }
+    breadthFirst() {
+        let arr = [];
+        let queue = [];
+        let current = this.root;
+
+        queue.push(current);
+        while (queue.length) {
+            current = queue.shift();
+            arr.push(current.val);
+            if (current.left) queue.push(current.left);
+            if (current.right) queue.push(current.right);
+        };
+
+        return arr;
+    }
 }
 
 module.exports = BinaryTree;
