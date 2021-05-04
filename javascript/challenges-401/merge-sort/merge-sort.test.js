15c534d96e05078fe5efde8c449f3099b174b5f0
boodah96 <2014990065@ses.yu.edu.jo>
2021-05-04 20:59:56 +0300

    cc27

--- javascript/challenges-401/merge-sort/merge-sort.test.js
+++ javascript/challenges-401/merge-sort/merge-sort.test.js
@@ -1,16 +1,13 @@
 'use strict';
+let { mergeSort, merge } = require('./merge-sort');
 
-function mergeSort(arr) {
-    let n = arr.length;
-    if (n <= 1) return arr;
-    let mid = Math.floor(n / 2);
-    let left = arr.splice(0, mid);
-    let right = arr;
+test('run mergeSort function with arr', () => {
+    let arr = [12, 1, 44, 2, 7];
+    expect(mergeSort(arr)).toEqual([1, 2, 7, 12, 44]);
 
-}
-
-test('run', () => {
-    mergeSort([12]);
-    expect('ll').toBeString;
+})
+test('array withe uniqe index', () => {
+    let arr = [12];
+    expect(mergeSort(arr)).toEqual([12]);
 
 })
